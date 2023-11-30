25e85ba601facbb2c8c7bf9e6eda30130eda4455
Michael McLoughlin <mmcloughlin@gmail.com>
2023-11-30 14:15:17 -0500

    get smt out

--- cranelift/isle/veri/veri_engine/src/isla.rs
+++ cranelift/isle/veri/veri_engine/src/isla.rs
@@ -3,22 +3,23 @@ use clap::Parser;
 use cranelift_codegen::print_errors::pretty_error;
 use cranelift_codegen::Context;
 use cranelift_reader::{parse_sets_and_triple, parse_test, ParseOptions};
-use isla_lib::config::ISAConfig;
-use isla_lib::smt::Checkpoint;
-use sha2::{Digest, Sha256};
-use std::fs::File;
-use std::io::{self, Read};
-use std::path::{Path, PathBuf};
-
 use crossbeam::queue::SegQueue;
-use isla_lib::bitvector::b64::B64;
-use isla_lib::bitvector::BV;
+use isla_lib::bitvector::{b129::B129, BV};
+use isla_lib::config::ISAConfig;
+use isla_lib::error::IslaError;
 use isla_lib::executor::{self, LocalFrame, TaskState};
 use isla_lib::init::initialize_architecture;
 use isla_lib::ir::{AssertionMode, Def, IRTypeInfo, Name, Symtab, Val};
 use isla_lib::ir_lexer::new_ir_lexer;
 use isla_lib::ir_parser;
 use isla_lib::memory::Memory;
+use isla_lib::simplify::{self, WriteOpts};
+use isla_lib::smt::{self, Checkpoint, Event, Solver};
+use sha2::{Digest, Sha256};
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufWriter, Read};
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 #[derive(Parser)]
@@ -64,12 +65,12 @@ fn main() -> anyhow::Result<()> {
     // Parse ISLA Architecture.
     let contents = read_to_string(&options.arch)?;
     let mut symtab = Symtab::new();
-    let mut arch = parse_ir::<B64>(&contents, &mut symtab)?;
+    let mut arch = parse_ir::<B129>(&contents, &mut symtab)?;
 
     // ISLA ISA Config.
     let mut hasher = Sha256::new();
     let type_info = IRTypeInfo::new(&arch);
-    let isa_config = match ISAConfig::<B64>::from_file(
+    let isa_config = match ISAConfig::<B129>::from_file(
         &mut hasher,
         &options.isa_config,
         None,
@@ -93,7 +94,7 @@ fn main() -> anyhow::Result<()> {
             symtab.clone(),
             type_info.clone(),
             &isa_config,
-        );
+        )?;
     }
 
     Ok(())
@@ -105,7 +106,7 @@ fn trace_function<B: BV>(
     symtab: Symtab,
     type_info: IRTypeInfo,
     isa_config: &ISAConfig<B>,
-) {
+) -> anyhow::Result<()> {
     let use_model_reg_init = true;
     let iarch = initialize_architecture(
         arch,
@@ -117,20 +118,25 @@ fn trace_function<B: BV>(
     );
     let shared_state = &&iarch.shared_state;
 
-    let opcode = Val::Bits(B::from_bytes(mc));
+    let initial_checkpoint = Checkpoint::new();
+    let solver_cfg = smt::Config::new();
+    let solver_ctx = smt::Context::new(solver_cfg);
+    let mut solver = Solver::from_checkpoint(&solver_ctx, initial_checkpoint);
+    let checkpoint = smt::checkpoint(&mut solver);
+
+    let opcode = mc[..4].try_into().expect("machine code has wrong length");
+    let opcode = Val::Bits(B::from_u32(u32::from_le_bytes(opcode)));
 
     let footprint_function = "zisla_footprint";
     let function_id = shared_state.symtab.lookup(&footprint_function);
     let (args, ret_ty, instrs) = shared_state.functions.get(&function_id).unwrap();
     let memory = Memory::new();
     let task_state = TaskState::<B>::new();
-    let initial_checkpoint = Checkpoint::new();
     let task = LocalFrame::new(function_id, args, ret_ty, Some(&[opcode]), instrs)
         .add_lets(&iarch.lets)
         .add_regs(&iarch.regs)
         .set_memory(memory)
-        .task_with_checkpoint(0, &task_state, initial_checkpoint);
-    // task.set_stop_conditions(&stop_conditions);
+        .task_with_checkpoint(0, &task_state, checkpoint);
 
     let num_threads = 1;
     let queue = Arc::new(SegQueue::new());
@@ -142,6 +148,47 @@ fn trace_function<B: BV>(
         queue.clone(),
         &executor::trace_collector,
     );
+
+    loop {
+        match queue.pop() {
+            Some(Ok((_, mut events))) => {
+                simplify::hide_initialization(&mut events);
+                simplify::remove_unused(&mut events);
+                simplify::propagate_forwards_used_once(&mut events);
+                simplify::commute_extract(&mut events);
+                simplify::eval(&mut events);
+
+                let events: Vec<Event<B>> = events.drain(..).rev().collect();
+                let stdout = std::io::stdout();
+                // Traces can be large, so use a 5MB buffer
+                let mut handle = BufWriter::with_capacity(5 * usize::pow(2, 20), stdout.lock());
+                let write_opts = WriteOpts::default();
+                simplify::write_events_with_opts(&mut handle, &events, &shared_state, &write_opts)
+                    .unwrap();
+                handle.flush().unwrap()
+            }
+
+            // Error during execution
+            Some(Err(err)) => {
+                let msg = format!("{}", err);
+                eprintln!(
+                    "{}",
+                    err.source_loc().message::<PathBuf>(
+                        None,
+                        shared_state.symtab.files(),
+                        &msg,
+                        true,
+                        true
+                    )
+                );
+                anyhow::bail!("{}", err);
+            }
+            // Empty queue
+            None => break,
+        }
+    }
+
+    Ok(())
 }
 
 fn parse_ir<'a, 'input, B: BV>(
