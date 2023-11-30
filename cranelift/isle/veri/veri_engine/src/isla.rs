1c8692a234f33ca8b52e4e98076fd4ff9f8b43c7
Michael McLoughlin <mmcloughlin@gmail.com>
2023-11-30 00:12:29 -0500

    integrate isla

--- cranelift/isle/veri/veri_engine/src/isla.rs
+++ cranelift/isle/veri/veri_engine/src/isla.rs
@@ -3,9 +3,18 @@ use clap::Parser;
 use cranelift_codegen::print_errors::pretty_error;
 use cranelift_codegen::Context;
 use cranelift_reader::{parse_sets_and_triple, parse_test, ParseOptions};
+use isla_lib::config::ISAConfig;
 use std::fs::File;
 use std::io::{self, Read};
 use std::path::{Path, PathBuf};
+
+// use isla_lib::executor;
+// use isla_lib::executor::{LocalFrame, StopAction, StopConditions, TaskState};
+use isla_lib::bitvector::b64::B64;
+use isla_lib::bitvector::BV;
+use isla_lib::ir::{Def, IRTypeInfo, Name, Symtab};
+use isla_lib::ir_lexer::new_ir_lexer;
+use isla_lib::ir_parser;
 
 #[derive(Parser)]
 struct Options {
@@ -16,6 +25,10 @@ struct Options {
     /// Specify the Cranelift target.
     #[clap(long = "target")]
     target: String,
+
+    /// Architecture definition.
+    #[clap(long)]
+    arch: PathBuf,
 
     /// Specify an input file to be used. Use '-' for stdin.
     file: PathBuf,
@@ -39,6 +52,11 @@ fn main() -> anyhow::Result<()> {
         Some(isa) => isa,
     };
 
+    // Parse ISLA Architecture.
+    let contents = read_to_string(&options.arch)?;
+    let mut symtab = Symtab::new();
+    let arch = parse_ir::<B64>(&contents, &mut symtab)?;
+
     // Compile functions.
     for (func, _) in test_file.functions {
         let mut context = Context::for_function(func);
@@ -46,10 +64,67 @@ fn main() -> anyhow::Result<()> {
         let _compiled_code = context
             .compile_and_emit(isa, &mut mem, &mut Default::default())
             .map_err(|err| anyhow::anyhow!("{}", pretty_error(&err.func, err.inner)))?;
-        println!("bytes: {:?}", mem);
+        trace_function(&mem, &arch);
     }
 
     Ok(())
+}
+
+fn trace_function<B: BV>(mc: &[u8], arch: &Vec<Def<Name, B>>) {
+    // &mut arch,
+    // symtab,
+
+    // type_info,
+    let _type_info = IRTypeInfo::new(&arch);
+
+    // TODO: &isa_config,
+    // TODO: assertion_mode,
+    // TODO: use_model_reg_init,
+
+    //let iarch = initialize_architecture(
+    //    &mut arch,
+    //    symtab,
+    //    type_info,
+    //    &isa_config,
+    //    assertion_mode,
+    //    use_model_reg_init,
+    //);
+
+    // let function_id = shared_state.symtab.lookup(&footprint_function);
+    // let (args, ret_ty, instrs) = shared_state.functions.get(&function_id).unwrap();
+    // let task_state = TaskState::new().with_reset_registers(reset_registers);
+    // let mut task = LocalFrame::new(
+    //     function_id,
+    //     args,
+    //     ret_ty,
+    //     Some(&[opcode_val.clone()]),
+    //     instrs,
+    // )
+    // .add_lets(lets)
+    // .add_regs(regs)
+    // .set_memory(memory)
+    // .task_with_checkpoint(0, &task_state, initial_checkpoint);
+    // task.set_stop_conditions(&stop_conditions);
+
+    // let num_threads = 1;
+    // executor::start_multi(
+    //     num_threads,
+    //     None,
+    //     vec![task],
+    //     shared_state,
+    //     queue.clone(),
+    //     &executor::trace_collector,
+    // );
+}
+
+fn parse_ir<'a, 'input, B: BV>(
+    contents: &'input str,
+    symtab: &'a mut Symtab<'input>,
+) -> anyhow::Result<Vec<Def<Name, B>>> {
+    match ir_parser::IrParser::new().parse(symtab, new_ir_lexer(&contents)) {
+        Ok(ir) => Ok(ir),
+        Err(_) => Err(anyhow::Error::msg("bad")),
+    }
 }
 
 /// Read an entire file into a string.
