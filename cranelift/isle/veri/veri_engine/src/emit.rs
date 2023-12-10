173bb50674dbd549d3b1efe96f313fe5600367cf
Michael McLoughlin <mmcloughlin@gmail.com>
2023-12-10 13:21:33 -0500

    veri: slight refactor isla emit

--- cranelift/isle/veri/veri_engine/src/emit.rs
+++ cranelift/isle/veri/veri_engine/src/emit.rs
@@ -5,11 +5,11 @@ use cranelift_codegen::settings;
 use cranelift_codegen::MachBuffer;
 use cranelift_codegen::MachInstEmit;
 use crossbeam::queue::SegQueue;
-use isla_lib::bitvector::{b129::B129, BV};
+use isla_lib::bitvector::{b64::B64, BV};
 use isla_lib::config::ISAConfig;
 use isla_lib::error::IslaError;
 use isla_lib::executor::{self, LocalFrame, TaskState};
-use isla_lib::init::initialize_architecture;
+use isla_lib::init::{initialize_architecture, Initialized};
 use isla_lib::ir::{AssertionMode, Def, IRTypeInfo, Name, Symtab, Val};
 use isla_lib::ir_lexer::new_ir_lexer;
 use isla_lib::ir_parser;
@@ -40,12 +40,12 @@ fn main() -> anyhow::Result<()> {
     // Parse ISLA Architecture.
     let contents = read_to_string(&options.arch)?;
     let mut symtab = Symtab::new();
-    let mut arch = parse_ir::<B129>(&contents, &mut symtab)?;
+    let mut arch = parse_ir::<B64>(&contents, &mut symtab)?;
 
     // ISLA ISA Config.
     let mut hasher = Sha256::new();
     let type_info = IRTypeInfo::new(&arch);
-    let isa_config = match ISAConfig::<B129>::from_file(
+    let isa_config = match ISAConfig::<B64>::from_file(
         &mut hasher,
         &options.isa_config,
         None,
@@ -55,6 +55,17 @@ fn main() -> anyhow::Result<()> {
         Ok(isa_config) => isa_config,
         Err(msg) => anyhow::bail!(msg),
     };
+
+    let use_model_reg_init = true;
+    let iarch = initialize_architecture(
+        &mut arch,
+        symtab,
+        type_info,
+        &isa_config,
+        AssertionMode::Optimistic,
+        use_model_reg_init,
+    );
+    let shared_state = &&iarch.shared_state;
 
     // Assemble an instruction.
     let inst = Inst::AluRRR {
@@ -66,17 +77,19 @@ fn main() -> anyhow::Result<()> {
     };
 
     let opcodes = opcodes(&inst);
-    println!("opcode = {:?}", opcodes);
+    assert_eq!(opcodes.len(), 1);
 
     // ISLA trace.
-    assert_eq!(opcodes.len(), 1);
-    trace_opcode(
-        opcodes[0],
-        &mut arch,
-        symtab.clone(),
-        type_info.clone(),
-        &isa_config,
-    )?;
+    let paths = trace_opcode(opcodes[0], &iarch)?;
+
+    // Dump.
+    for events in paths {
+        let stdout = std::io::stdout();
+        let mut handle = BufWriter::with_capacity(5 * usize::pow(2, 20), stdout.lock());
+        let write_opts = WriteOpts::default();
+        simplify::write_events_with_opts(&mut handle, &events, &shared_state, &write_opts).unwrap();
+        handle.flush().unwrap();
+    }
 
     Ok(())
 }
@@ -100,22 +113,10 @@ fn opcodes(inst: &Inst) -> Vec<u32> {
     opcodes
 }
 
-fn trace_opcode<B: BV>(
+fn trace_opcode<'ir, B: BV>(
     opcode: u32,
-    arch: &mut Vec<Def<Name, B>>,
-    symtab: Symtab,
-    type_info: IRTypeInfo,
-    isa_config: &ISAConfig<B>,
-) -> anyhow::Result<()> {
-    let use_model_reg_init = true;
-    let iarch = initialize_architecture(
-        arch,
-        symtab,
-        type_info,
-        &isa_config,
-        AssertionMode::Optimistic,
-        use_model_reg_init,
-    );
+    iarch: &'ir Initialized<'ir, B>,
+) -> anyhow::Result<Vec<Vec<Event<B>>>> {
     let shared_state = &&iarch.shared_state;
 
     let initial_checkpoint = Checkpoint::new();
@@ -148,22 +149,21 @@ fn trace_opcode<B: BV>(
         &executor::trace_collector,
     );
 
+    let mut paths = Vec::new();
     loop {
         match queue.pop() {
             Some(Ok((_, mut events))) => {
                 simplify::hide_initialization(&mut events);
+                simplify::remove_extra_register_fields(&mut events);
+                simplify::remove_repeated_register_reads(&mut events);
+                simplify::remove_unused_register_assumptions(&mut events);
                 simplify::remove_unused(&mut events);
                 simplify::propagate_forwards_used_once(&mut events);
                 simplify::commute_extract(&mut events);
                 simplify::eval(&mut events);
 
                 let events: Vec<Event<B>> = events.drain(..).rev().collect();
-                let stdout = std::io::stdout();
-                let mut handle = BufWriter::with_capacity(5 * usize::pow(2, 20), stdout.lock());
-                let write_opts = WriteOpts::default();
-                simplify::write_events_with_opts(&mut handle, &events, &shared_state, &write_opts)
-                    .unwrap();
-                handle.flush().unwrap()
+                paths.push(events);
             }
 
             // Error during execution
@@ -186,7 +186,7 @@ fn trace_opcode<B: BV>(
         }
     }
 
-    Ok(())
+    Ok(paths)
 }
 
 fn parse_ir<'a, 'input, B: BV>(
