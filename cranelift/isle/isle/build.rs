dc155ce6340984e41a81f456e3e9c366b93ee728
Michael McLoughlin <mmcloughlin@gmail.com>
2023-12-30 18:40:23 -0500

    test against full isle files

--- cranelift/isle/isle/build.rs
+++ cranelift/isle/isle/build.rs
@@ -1,4 +1,4 @@
-use std::fmt::Write;
+use std::{fmt::Write, path::PathBuf};
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
@@ -14,14 +14,21 @@ fn main() {
     emit_tests(&mut out, "isle_examples/fail", "run_fail");
     emit_tests(&mut out, "isle_examples/link", "run_link");
     emit_tests(&mut out, "isle_examples/run", "run_run");
+
     emit_tests(&mut out, "isle_examples/pass", "run_print");
+    emit_tests(&mut out, "../../codegen/src/opts", "run_print");
+    emit_tests(&mut out, "../../codegen/src/isa/x64", "run_print");
+    emit_tests(&mut out, "../../codegen/src/isa/aarch64", "run_print");
+    emit_tests(&mut out, "../../codegen/src/isa/riscv64", "run_print");
 
     let output = out_dir.join("isle_tests.rs");
     std::fs::write(output, out).unwrap();
 }
 
 fn emit_tests(out: &mut String, dir_name: &str, runner_func: &str) {
-    for test_file in std::fs::read_dir(dir_name).unwrap() {
+    let dir_path = PathBuf::from(dir_name);
+    let test_name = dir_path.file_name().unwrap().to_string_lossy();
+    for test_file in std::fs::read_dir(&dir_path).unwrap() {
         let test_file = test_file.unwrap().file_name().into_string().unwrap();
         if !test_file.ends_with(".isle") {
             continue;
@@ -29,7 +36,12 @@ fn emit_tests(out: &mut String, dir_name: &str, runner_func: &str) {
         let test_file_base = test_file.replace(".isle", "");
 
         writeln!(out, "#[test]").unwrap();
-        writeln!(out, "fn test_{}_{}() {{", runner_func, test_file_base).unwrap();
+        writeln!(
+            out,
+            "fn test_{}_{}_{}() {{",
+            runner_func, test_name, test_file_base
+        )
+        .unwrap();
         writeln!(out, "    {}(\"{}/{}\");", runner_func, dir_name, test_file).unwrap();
         writeln!(out, "}}").unwrap();
     }
